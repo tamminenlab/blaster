108f68c42b8ef9c333f7cf95e466c804ddacfc10
Manu Tamminen <mavata@gmail.com>
2021-01-12 16:34:04 +0200

    Preparing BLAST functionality

--- src/blast.cpp
+++ src/blast.cpp
@@ -1,13 +1,118 @@
 #include <Rcpp.h>
 #include <string>
+#include <memory>
 
 #include "FASTA/Reader.h"
 #include "Alphabet/DNA.h"
 #include "FileFormat.h"
+#include "Database.h"
+#include "Database/HitWriter.h"
+#include "Database/GlobalSearch.h"
+#include "Sequence.h"
+#include "Alphabet/DNA.h"
+#include "Alphabet/Protein.h"
+
+#include "Common.h"
+#include "FileFormat.h"
+#include "WorkerQueue.h"
 
 using namespace Rcpp;
 
+
+template < typename A >
+using QueryWithHits     = std::pair< Sequence< A >, HitList< A > >;
+
+template < typename A >
+using QueryWithHitsList = std::deque< QueryWithHits< A > >;
+
+template < typename A >
+class QueueItemInfo< QueryWithHitsList< A > > {
+public:
+  static size_t Count( const QueryWithHitsList< A >& list ) {
+    return std::accumulate(
+                           list.begin(), list.end(), 0,
+                           []( int sum, const QueryWithHits< A >& q ) { return sum + q.second.size(); } );
+  }
+};
+
+template < typename A >
+class SearchResultsWriterWorker {
+public:
+  SearchResultsWriterWorker( const std::string& path )
+    : mWriter( std::move(
+                         DetectFileFormatAndOpenHitWriter< A >( path, FileFormat::ALNOUT ) ) ) {}
+
+  void Process( const QueryWithHitsList< A >& queryWithHitsList ) {
+    for( auto& queryWithHits : queryWithHitsList ) {
+      ( *mWriter ) << queryWithHits;
+    }
+  }
+
+private:
+  std::unique_ptr< HitWriter< A > > mWriter;
+};
+
+template < typename A >
+using SearchResultsWriter =
+  WorkerQueue< SearchResultsWriterWorker< A >, QueryWithHitsList< A >,
+               const std::string& >;
+
+template < typename A >
+class QueueItemInfo< SequenceList< A > > {
+public:
+  static size_t Count( const SequenceList< A >& list ) {
+    return list.size();
+  }
+};
+
+template < typename A >
+class QueryDatabaseSearcherWorker {
+public:
+  QueryDatabaseSearcherWorker( SearchResultsWriter< A >* writer,
+                               const Database< A >*      database,
+                               const SearchParams< A > &params )
+    : mWriter( *writer ),
+      mGlobalSearch( *database, params ) {}
+
+  void Process( const SequenceList< A >& queries ) {
+    QueryWithHitsList< A > list;
+
+    for( auto& query : queries ) {
+      auto hits = mGlobalSearch.Query( query );
+      if( hits.empty() )
+        continue;
+
+      list.push_back( { query, hits } );
+    }
+
+    if( !list.empty() ) {
+      mWriter.Enqueue( list );
+    }
+  }
+
+private:
+  GlobalSearch< A >         mGlobalSearch;
+  SearchResultsWriter< A >& mWriter;
+};
+
+template < typename A >
+using QueryDatabaseSearcher =
+  WorkerQueue< QueryDatabaseSearcherWorker< A >, SequenceList< A >,
+               SearchResultsWriter< A >*, const Database< A >*,
+               const SearchParams< A >& >;
+
+template < typename A >
+struct WordSize {
+  static const int VALUE = 8; // DNA, default
+};
+
+template <>
+struct WordSize< Protein > {
+  static const int VALUE = 5;
+};
+
 // [[Rcpp::export]]
+
 DataFrame blast(DataFrame seq_table) 
 {
   std::vector< std::string > ids = seq_table["Id"];
@@ -22,7 +127,9 @@ DataFrame blast(DataFrame seq_table)
     content << ">" << id << "\n" << seq << "\n";
   }
 
-  std::istringstream iss( content.str() );
+  std::string fasta_string( content.str() );
+
+  std::istringstream iss( fasta_string );
 
   std::unique_ptr< SequenceReader< DNA > > dbReader( new FASTA::Reader< DNA >( iss ) );
   
@@ -34,15 +141,88 @@ DataFrame blast(DataFrame seq_table)
     sequences.push_back( std::move( seq ) );
   }
 
-  std::vector< int > new_ids;
-  std::vector< std::string > new_seqs;
-  int counter{1};
-  for (Sequence< DNA > i : sequences) {
-    new_ids.push_back( counter );
-    new_seqs.push_back( i.sequence );
-    ++counter;
-      }
+  ProgressOutput progress;
 
-  return DataFrame::create(Named("Id") = new_ids,
-                           Named("Seq") = new_seqs);
+  enum ProgressType {
+                     ReadDBFile,
+                     StatsDB,
+                     IndexDB,
+                     ReadQueryFile,
+                     SearchDB,
+                     WriteHits
+  };
+
+  progress.Add( ProgressType::ReadDBFile, "Read database", UnitType::BYTES );
+  progress.Add( ProgressType::StatsDB, "Analyze database" );
+  progress.Add( ProgressType::IndexDB, "Index database" );
+  progress.Add( ProgressType::ReadQueryFile, "Read queries", UnitType::BYTES );
+  progress.Add( ProgressType::SearchDB, "Search database" );
+  progress.Add( ProgressType::WriteHits, "Write hits" );
+
+  // Read DB
+  progress.Activate( ProgressType::ReadDBFile );
+  while( !dbReader->EndOfFile() ) {
+    (*dbReader) >> seq;
+    sequences.push_back( std::move( seq ) );
+    progress.Set( ProgressType::ReadDBFile, dbReader->NumBytesRead(),
+                  dbReader->NumBytesTotal() );
+  }
+
+  // Index DB
+  Database< DNA > db( WordSize< DNA >::VALUE );
+  db.SetProgressCallback(
+                         [&]( typename Database< DNA >::ProgressType type, size_t num, size_t total ) {
+                           switch( type ) {
+                           case Database< DNA >::ProgressType::StatsCollection:
+                             progress.Activate( ProgressType::StatsDB )
+                               .Set( ProgressType::StatsDB, num, total );
+                             break;
+
+                           case Database< DNA >::ProgressType::Indexing:
+                             progress.Activate( ProgressType::IndexDB )
+                               .Set( ProgressType::IndexDB, num, total );
+                             break;
+
+                           default:
+                             break;
+                           }
+                         } );
+  db.Initialize( sequences );
+
+  // Read and process queries
+  const int numQueriesPerWorkItem = 64;
+  
+  SearchParams< DNA > searchParams;
+
+  SearchResultsWriter< DNA >   writer( 1, "koe.csv" );
+  QueryDatabaseSearcher< DNA > searcher( -1, &writer, &db, searchParams );
+
+  searcher.OnProcessed( [&]( size_t numProcessed, size_t numEnqueued ) {
+                          progress.Set( ProgressType::SearchDB, numProcessed, numEnqueued );
+                        } );
+  writer.OnProcessed( [&]( size_t numProcessed, size_t numEnqueued ) {
+                        progress.Set( ProgressType::WriteHits, numProcessed, numEnqueued );
+                      } );
+
+  std::istringstream iss2( fasta_string );
+  std::unique_ptr< SequenceReader< DNA > > qryReader( new FASTA::Reader< DNA >( iss2 ) );
+
+  SequenceList< DNA > queries;
+  progress.Activate( ProgressType::ReadQueryFile );
+  while( !qryReader->EndOfFile() ) {
+    qryReader->Read( numQueriesPerWorkItem, &queries );
+    searcher.Enqueue( queries );
+    progress.Set( ProgressType::ReadQueryFile, qryReader->NumBytesRead(),
+                  qryReader->NumBytesTotal() );
+  }
+
+  // Search
+  progress.Activate( ProgressType::SearchDB );
+  searcher.WaitTillDone();
+
+  progress.Activate( ProgressType::WriteHits );
+  writer.WaitTillDone();
+
+  return DataFrame::create(Named("Id") = ids,
+                           Named("Seq") = seqs);
 }
